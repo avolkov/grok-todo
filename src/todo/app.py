350e3a078a7860d7ba330dbd91c3991d786119ea
Alex Volkov <alex@flamy.ca>
2013-02-18 14:55:31 -0500

    Added form to todo list

--- src/todo/app.py
+++ src/todo/app.py
@@ -3,12 +3,34 @@ import grok
 from todo import resource
 
 class Todo(grok.Application, grok.Container):
-    todolists = [{
-        'title':'Daily tasks for Grok',
-        'description':'A list of tasks that Grok does everyday',
-        'items':['Clean cave', 'Hunt breakfast', 'Sharpen ax']
-        }]
+    todolists = []
 
 class Index(grok.View):
     def update(self):
-        resource.style.need()
+        form = self.request.form
+        if 'new_list' in form:
+            title = form['list_title']
+            description = form['list_description']
+            self.context.todolists.append({'title':title, 
+                                           'description':description,
+                                           'items':[]
+                                         })
+            return
+        if 'list_index' in form:
+            index = int(form['list_index'])
+            items = self.context.todolists[index]['items']
+            if 'new_item' in form:
+                description = form['item_description']
+                items.append(({'description':description, 'checked':False}))
+                return
+            elif 'update_list' in form:
+                for item in range(len(items)):
+                    if 'item_%s' % item in form:
+                        items[item]['checked'] = True
+                    else:
+                        items[item]['checked'] = False
+                return
+            else:
+                for item in range(len(items)):
+                    if 'delete_%s' % item in form:
+                        items.remove(items[item])
