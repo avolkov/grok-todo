646ae6b35b2ddf336ce763c4f4b4f8523e416d34
Alex Volkov <alex@flamy.ca>
2013-02-18 13:44:51 -0500

    Modified sample page to display todo lists.

--- src/todo/app.py
+++ src/todo/app.py
@@ -3,7 +3,11 @@ import grok
 from todo import resource
 
 class Todo(grok.Application, grok.Container):
-    pass
+    todolists = [{
+        'title':'Daily tasks for Grok',
+        'description':'A list of tasks that Grok does everyday',
+        'items':['Clean cave', 'Hunt breakfast', 'Sharpen ax']
+        }]
 
 class Index(grok.View):
     def update(self):
